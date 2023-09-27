589575681ba7e632edc7c0dd6d4df468c2ecb3a6
2harsh <harsh.singh@flexcar.com>
2023-09-27 20:51:51 +0530

    three

--- lambda/get-counter.js
+++ lambda/get-counter.js
@@ -1,13 +1,18 @@
 // lambda/get-counter.js
-
-const express = require('express');
-const app = express();
-
-let counter = 0;
-
-app.get('/get-counter', (req, res) => {
-  res.json({ counter });
-});
-
-// Export the Express app for Netlify Functions
-module.exports = app;
+exports.handler = async (event, context) => {
+    try {
+      // Your code to retrieve the counter value here
+      const counterValue = 42; // Replace with your logic to get the counter value
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ counter: counterValue }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' }),
+      };
+    }
+  };
+  
