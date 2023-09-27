53fcb51a65355a4c1e487ebab58e25313af1272f
2harsh <harsh.singh@flexcar.com>
2023-09-27 20:58:05 +0530

    five

--- lambda/increment.js
+++ lambda/increment.js
@@ -1,30 +1,20 @@
 // lambda/increment.js
 
-const express = require('express');
-const bodyParser = require('body-parser'); // Import body-parser to parse JSON data
+exports.handler = async (event, context) => {
+  try {
+    // Your code to increment the counter here
+    // Example: Increment a counter value
+    let counter = 0; // Replace with your logic to get and update the counter value
+    counter++;
 
-const app = express();
-
-// Middleware to parse JSON data from POST requests
-app.use(bodyParser.json());
-
-let counter = 0;
-
-app.post('/increment', (req, res) => {
-  // Check if the request body contains valid JSON data with an "amount" property
-  if (req.body && req.body.amount) {
-    const amount = parseInt(req.body.amount);
-
-    if (!isNaN(amount)) {
-      counter += amount;
-      res.json({ message: `Counter incremented by ${amount}` });
-    } else {
-      res.status(400).json({ error: 'Invalid amount' });
-    }
-  } else {
-    res.status(400).json({ error: 'Request body must contain an "amount" property' });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ counter: counter }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
   }
-});
-
-// Export the Express app for Netlify Functions
-module.exports = app;
+};
